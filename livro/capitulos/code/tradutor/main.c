2a523d3bae41ffa12ca72251ddc775d48639bddf
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-05-16 16:41:41 -0300

    código fonte incorporado

---
+++ livro/capitulos/code/tradutor/main.c
@@ -0,0 +1,9 @@
+#include "teste.h"
+#include <stdio.h>
+
+int main(){
+  int a, b;
+  a = soma(2, 3);
+  printf("Soma = %d\n", a);
+}
+
---
+++ livro/capitulos/code/tradutor/teste.h
@@ -0,0 +1,6 @@
+//Soma números
+int soma(int op1, int op2);
+
+//Subtrai numeros
+int subtrai(int op1, int op2);
+
